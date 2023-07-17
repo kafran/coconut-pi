2bc4ac177e091a51538d6220e6e844f3fda53b2c
Kolmar Kafran <kolmar@kafran.net>
2023-07-17 18:42:41 -0300

    Server-Sent Event

--- app/main.go
+++ app/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 func piHandler(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("vcgencmd", "measure_temp")
-	output, err := cmd.Output()
-	if err != nil {
-		http.Error(w, "Failed to execute command: " + err.Error(), http.StatusInternalServerError)
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming not supported!", http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte("Current temperature: " + string(output) + "°C"))
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	for {
+		cmd := exec.Command("vcgencmd", "measure_temp")
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Fprintf(w, "data: Error: %v\n\n", err)
+			flusher.Flush()
+			time.Sleep(time.Second)
+			continue
+		}
+
+		temp := strings.TrimSuffix(string(output), "'C\n")
+		temp = strings.TrimPrefix(temp, "temp=")
+
+		fmt.Fprintf(w, "data: Current temperature: %s°C\n\n", temp)
+		flusher.Flush()
+		time.Sleep(time.Second)
+	}
 }
 
 func main() {
-	http.HandleFunc("/pi", piHandler)
+	http.Handle("/", http.FileServer(http.Dir("./app/views/")))
+	http.HandleFunc("/temp", piHandler)
 	http.ListenAndServe(":8080", nil)
 }
