c9ecbb015ec187c55d23780e9e5bf13310f65f40
Kolmar Kafran <kolmar@kafran.net>
2023-07-17 17:05:25 -0300

    Simple http server

--- app/main.go
+++ app/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os/exec"
+)
+
+func piHandler(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.Command("vcgencmd", "measure_temp")
+	output, err := cmd.Output()
+	if err != nil {
+		http.Error(w, "Failed to execute command: " + err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("Current temperature: " + string(output) + "°C"))
+}
 
 func main() {
-	fmt.Println("Hello, Go!")
+	http.HandleFunc("/pi", piHandler)
+	http.ListenAndServe(":8080", nil)
 }
