a829a0658dc64e07819056e91c4fc60b82f37f47
Kolmar Kafran <kolmar@kafran.net>
2023-07-20 14:35:55 -0300

    Refactor metrics monitor

--- app/main.go
+++ app/main.go
@@ -59,81 +59,120 @@ func (p *Publisher) Publish(event Event) {
 	for subscriber := range p.Subscribers {
 		select {
 		case subscriber.Events <- event:
-			// fmt.Println("Escreve evento ", event)
 		default:
 			fmt.Println("Skipping event ", event)
 		}
 	}
 }
 
-// TODO - Improve this.
-func monitor() {
-	for {
-		for _, event := range []string{"temp", "clock", "volt", "mem"} {
-			var cmd *exec.Cmd
-			switch event {
-			case "temp":
-				cmd = exec.Command("vcgencmd", "measure_temp")
-			case "clock":
-				cmd = exec.Command("vcgencmd", "measure_clock", "arm")
-			case "volt":
-				cmd = exec.Command("vcgencmd", "measure_volts", "core")
-			case "mem":
-				cmd = exec.Command("free", "-h")
-			}
+func (p *Publisher) Size() int {
+	p.Mu.RLock()
+	defer p.Mu.RUnlock()
+	return len(p.Subscribers)
+}
 
-			output, err := cmd.Output()
-			if err != nil {
-				p.Publish(Event{event, "-"})
-				fmt.Println(Event{event, fmt.Sprintf("Error: %v", err)})
-				continue
-			}
+type PiMetric struct {
+	Name         string
+	Command      []string
+	FormatOutput func(output string) (string, error)
+}
 
+var piMetrics = []PiMetric{
+	{
+		Name:    "temp",
+		Command: []string{"vcgencmd", "measure_temp"},
+		FormatOutput: func(output string) (string, error) {
 			var formattedOutput string
-			switch event {
-			case "temp":
-				formattedOutput = strings.TrimSuffix(string(output), "'C\n")
-				formattedOutput = strings.TrimPrefix(formattedOutput, "temp=")
-				formattedOutput = fmt.Sprintf("%s °C", formattedOutput)
-			case "clock":
-				formattedOutput = strings.TrimSuffix(string(output), "\n")
-				formattedOutput = strings.Split(formattedOutput, "=")[1]
-				hz, err := strconv.ParseFloat(formattedOutput, 64)
-				if err != nil {
-					p.Publish(Event{event, fmt.Sprintf("Error: %v", err)})
-					continue
-				}
-				ghz := hz / 1e9
-				formattedOutput = fmt.Sprintf("%.2f GHz", ghz)
-			case "volt":
-				formattedOutput = strings.TrimSuffix(string(output), "V\n")
-				formattedOutput = strings.TrimPrefix(formattedOutput, "volt=")
-				formattedOutput = fmt.Sprintf("%s V", formattedOutput)
-			case "mem":
-				lines := strings.Split(string(output), "\n")
-				if len(lines) < 2 {
-					p.Publish(Event{event, "Error: unexpected output format"})
-					continue
-				}
-				memInfo := strings.Fields(lines[1])
-				if len(memInfo) < 3 {
-					p.Publish(Event{event, "Error: unexpected output format"})
-					continue
-				}
-				formattedOutput = fmt.Sprintf("%s / %s", memInfo[2], memInfo[1])
+			formattedOutput = strings.TrimSuffix(output, "'C\n")
+			formattedOutput = strings.TrimPrefix(formattedOutput, "temp=")
+			formattedOutput = fmt.Sprintf("%s °C", formattedOutput)
+			return formattedOutput, nil
+		},
+	},
+	{
+		Name:    "clock",
+		Command: []string{"vcgencmd", "measure_clock", "arm"},
+		FormatOutput: func(output string) (string, error) {
+			var formattedOutput string
+			formattedOutput = strings.TrimSuffix(output, "\n")
+			formattedOutput = strings.Split(formattedOutput, "=")[1]
+			hz, err := strconv.ParseFloat(formattedOutput, 64)
+			if err != nil {
+				return "-", err
 			}
-			p.Publish(Event{event, formattedOutput})
-			//fmt.Printf("Event: %v\n", Event{event, formattedOutput})
+			ghz := hz / 1e9
+			formattedOutput = fmt.Sprintf("%.2f GHz", ghz)
+			return formattedOutput, nil
+		},
+	},
+	{
+		Name:    "volt",
+		Command: []string{"vcgencmd", "measure_volts", "core"},
+		FormatOutput: func(output string) (string, error) {
+			var formattedOutput string
+			formattedOutput = strings.TrimSuffix(output, "V\n")
+			formattedOutput = strings.TrimPrefix(formattedOutput, "volt=")
+			formattedOutput = fmt.Sprintf("%s V", formattedOutput)
+			return formattedOutput, nil
+		},
+	},
+	{
+		Name:    "mem",
+		Command: []string{"free", "-h"},
+		FormatOutput: func(output string) (string, error) {
+			var formattedOutput string
+			lines := strings.Split(output, "\n")
+			if len(lines) < 2 {
+				return "-", fmt.Errorf("Error: unexpected output format")
+			}
+			memInfo := strings.Fields(lines[1])
+			if len(memInfo) < 3 {
+				return "-", fmt.Errorf("Error: unexpected output format")
+			}
+			formattedOutput = fmt.Sprintf("%s / %s", memInfo[2], memInfo[1])
+			return formattedOutput, nil
+		},
+	},
+}
+
+func runPiMetric(metric PiMetric) {
+	for {
+		output, err := exec.Command(metric.Command[0], metric.Command[1:]...).Output()
+		if err != nil {
+			p.Publish(Event{metric.Name, "-"})
+			fmt.Println(Event{metric.Name, err.Error()})
+			continue
 		}
+		formattedOutput, err := metric.FormatOutput(string(output))
+		if err != nil {
+			p.Publish(Event{metric.Name, "-"})
+			fmt.Println(Event{metric.Name, err.Error()})
+			continue
+		}
+		p.Publish(Event{metric.Name, formattedOutput})
+		time.Sleep(time.Second)
+	}
+}
+
+func runObserversMetric() {
+	for {
 		var plural string
-		if len(p.Subscribers) > 1 {
+		observers := p.Size()
+		if observers > 1 {
 			plural = "people"
 		} else {
 			plural = "person"
 		}
-		p.Publish(Event{"observers", fmt.Sprintf("%d %s here", len(p.Subscribers), plural)})
-		time.Sleep(time.Second * 10)
+		p.Publish(Event{"observers", fmt.Sprintf("%d %s here", observers, plural)})
+		time.Sleep(time.Second)
 	}
+}
+
+func monitor() {
+	for _, metric := range piMetrics {
+		go runPiMetric(metric)
+	}
+	go runObserversMetric()
 }
 
 func piHandler(w http.ResponseWriter, r *http.Request) {
