9a4788498a83df39146057169cde86e7975c8e71
Kolmar Kafran <kolmar@kafran.net>
2023-07-20 03:47:28 -0300

    Change architecture to a naive pub/sub

--- app/main.go
+++ app/main.go
@@ -6,22 +6,62 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/kafran/coconut-pi/app/views"
 )
 
-func piHandler(w http.ResponseWriter, r *http.Request) {
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		http.Error(w, "Streaming not supported!", http.StatusInternalServerError)
-		return
+type Event struct {
+	Type string
+	Data string
+}
+
+type Subscriber struct {
+	Events chan Event
+}
+
+type Publisher struct {
+	Subscribers []Subscriber
+	Mu          sync.Mutex
+}
+
+func (p *Publisher) Subscribe() Subscriber {
+	p.Mu.Lock()
+	defer p.Mu.Unlock()
+
+	sub := Subscriber{
+		Events: make(chan Event),
 	}
+	p.Subscribers = append(p.Subscribers, sub)
+	fmt.Println("Subscriber subscribed ", sub)
+	return sub
+}
 
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
+func (p *Publisher) Unsubscribe(sub Subscriber) {
+	p.Mu.Lock()
+	defer p.Mu.Unlock()
 
+	for i, subscriber := range p.Subscribers {
+		if subscriber == sub {
+			p.Subscribers = append(p.Subscribers[:i], p.Subscribers[i+1:]...)
+			close(sub.Events)
+			fmt.Println("Subscriber unsubscribed ", sub)
+			break
+		}
+	}
+}
+
+func (p *Publisher) Publish(event Event) {
+	p.Mu.Lock()
+	defer p.Mu.Unlock()
+
+	for _, subscriber := range p.Subscribers {
+		subscriber.Events <- event
+	}
+}
+
+func monitor() {
 	for {
 		var cmd *exec.Cmd
 		for _, event := range []string{"temp", "clock", "volt"} {
@@ -36,9 +76,7 @@ func piHandler(w http.ResponseWriter, r *http.Request) {
 
 			output, err := cmd.Output()
 			if err != nil {
-				fmt.Fprintf(w, "event: %s\ndata: Error: %v\n\n", event, err)
-				flusher.Flush()
-				time.Sleep(time.Second)
+				p.Publish(Event{event, fmt.Sprintf("Error: %v", err)})
 				continue
 			}
 
@@ -53,9 +91,7 @@ func piHandler(w http.ResponseWriter, r *http.Request) {
 				formattedOutput = strings.Split(formattedOutput, "=")[1]
 				hz, err := strconv.ParseFloat(formattedOutput, 64)
 				if err != nil {
-					fmt.Fprintf(w, "event: %s\ndata: Error: %v\n\n", event, err)
-					flusher.Flush()
-					time.Sleep(time.Second)
+					p.Publish(Event{event, fmt.Sprintf("Error: %v", err)})
 					continue
 				}
 				ghz := hz / 1e9
@@ -65,16 +101,42 @@ func piHandler(w http.ResponseWriter, r *http.Request) {
 				formattedOutput = strings.TrimPrefix(formattedOutput, "volt=")
 				formattedOutput = fmt.Sprintf("%s V", formattedOutput)
 			}
+			p.Publish(Event{event, formattedOutput})
+		}
+		time.Sleep(time.Second)
+	}
+}
 
-			fmt.Fprintf(w, "event: %s\ndata: %s\n\n", event, formattedOutput)
+func piHandler(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming not supported!", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	sub := p.Subscribe()
+	defer p.Unsubscribe(sub)
+
+	for {
+		select {
+		case event := <-sub.Events:
+			fmt.Fprintf(w, "event: %s\ndata: %s\n\n", event.Type, event.Data)
 			flusher.Flush()
-			time.Sleep(time.Second)
+		case <-r.Context().Done():
+			return
 		}
 	}
 }
 
+var p = &Publisher{}
+
 func main() {
 	http.Handle("/", http.FileServer(http.FS(views.Files)))
 	http.HandleFunc("/status", piHandler)
+	go monitor()
 	http.ListenAndServe(":8080", nil)
 }
